3a098bab6ec756aff675e27d04bb1240a50862f6
JasonSather <jassather@gmail.com>
2021-12-14 18:14:54 -0800

    added face detection

--- src/hello-world.py
+++ src/hello-world.py
@@ -1,9 +1,26 @@
-from time import sleep
-from picamera import PiCamera
+import io
+import time
+import picamera
+import cv2
+import numpy as np
 
-camera = PiCamera()
-camera.resolution = (1024, 768)
-camera.start_preview()
-# Camera warm-up time
-sleep(2)
-camera.capture('./photos/First_Python_Pic.jpg')
+# Create the in-memory stream
+
+stream = io.BytesIO()
+with picamera.PiCamera() as camera:
+    camera.start_preview()
+    time.sleep(2)
+    camera.capture(stream, format='jpeg')
+
+data = np.frombuffer(stream.getvalue(), dtype=np.uint8)
+
+image = cv2.imdecode(data, 1)
+
+
+face_cascade = cv2.CascadeClassifier('./model/haarcascade_frontalface_default.xml')
+# image = cv2.imread("./forever_photos/sather.jpeg")
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+faces = face_cascade.detectMultiScale(gray, 1.1, 8)
+for (x, y, w, h) in faces:
+    cv2.rectangle(image, (x, y), (x+w, y+h), (255, 0, 0), 2)
+cv2.imwrite("./photos/sather.jpeg", image)
