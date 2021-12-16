f375ae74251dde7edf36606cf6727c68dc85f36d
JasonSather <jassather@gmail.com>
2021-12-16 14:33:18 -0800

    commented out image taken on startup

--- src/main.py
+++ src/main.py
@@ -4,15 +4,15 @@ from discord_bot import DiscordBot
 def main():
     """The main file will run everything
     """
-    cam = Camera()
-    stream = cam.getPictureStream()
-    faceFinder = FaceFinder()
-    numFaces = faceFinder.getNumFacesFromImage(stream)
-    print('we found '+ str(numFaces)+ ' faces')
+    # cam = Camera()
+    # stream = cam.getPictureStream()
+    # faceFinder = FaceFinder()
+    # numFaces = faceFinder.getNumFacesFromImage(stream)
+    # print('we found '+ str(numFaces)+ ' faces')
 
     #Start the discord bot...
     discordBot = DiscordBot()
-    discordBot.giveDripPic(stream)
+    #discordBot.giveDripPic(stream)
     discordBot.start()
     
 
