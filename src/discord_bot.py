ce4435965244b296f37172a33758b79e98be0278
JasonSather <jassather@gmail.com>
2021-12-16 14:51:44 -0800

    restructured file hierarchy

--- src/discord_bot.py
+++ src/discord_bot.py
@@ -2,7 +2,10 @@ import discord
 from discord.ext import commands
 from decouple import config
 import asyncio
+import sys
+sys.path.append('./src/imageProcessing')
 from camera import Camera
+from face_detector import FaceFinder
 
 class DiscordBot:
     def __init__(self) -> None:
@@ -47,9 +50,9 @@ class DiscordBot:
 
 
         @self.bot.command()
-        async def farewell(ctx):  #Be sure to disable this command or make it admin exclusive...
+        async def bye(ctx):  #Be sure to disable this command or make it admin exclusive...
             """Close the bot 3 seconds after it's ready, just for the sake of the example."""
-            await ctx.send("Bye")
+            await ctx.send("Farewell")
             await asyncio.sleep(3)
             await self.bot.close()
 
@@ -62,6 +65,20 @@ class DiscordBot:
             stream.seek(0)
             dripPic = discord.File(fp=stream, filename=str(filename)+'.jpeg', spoiler=False)
             await ctx.send(file = dripPic)
+
+        @self.bot.command()
+        async def orwell(ctx, filename = 'drip'):  #Be sure to disable this command or make it admin exclusive...
+            """Close the bot 3 seconds after it's ready, just for the sake of the example."""
+            await ctx.send("Making a orwell pic")
+            cam = Camera()
+            stream = cam.getPictureStream()
+            faceFinder = FaceFinder()
+            numFaces = faceFinder.getNumFacesFromImage(stream)
+            print('we found '+ str(numFaces)+ ' faces')
+            stream.seek(0)
+            dripPic = discord.File(fp=stream, filename=str(filename)+'.jpeg', spoiler=False)
+            await ctx.send(file = dripPic)
+
             
 
     
