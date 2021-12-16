3b9cb301f92760b37a9a9be1e38a326c6f111a11
JasonSather <jassather@gmail.com>
2021-12-15 21:18:48 -0800

    adding chatting functionality

--- src/discord_bot.py
+++ src/discord_bot.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
-import random
-
+from decouple import config
+import asyncio
 description = '''Coolest Boba Bot ever.
 
 (Tu won't let me call her an e-girl)'''
@@ -11,12 +11,12 @@ intents = discord.Intents.default()
 bot = commands.Bot(command_prefix='?', description=description, intents=intents)
 
 
-# @bot.event
-# async def on_ready():
-#     print('Logged in as')
-#     print(bot.user.name)
-#     print(bot.user.id)
-#     print('------')
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
 
 
 @bot.command()
@@ -24,5 +24,21 @@ async def add(ctx, left: int, right: int):
     """Adds two numbers together."""
     await ctx.send(left + right)
 
-bot.run('token')
+@bot.command()
+async def checkStore(ctx):
+    """Tells how many people are in the store"""
+    await ctx.send("Can't see the store right now :(")
+
+
+
+@bot.command()
+async def farewell(ctx):  #Be sure to disable this command or make it admin exclusive...
+    """Close the bot 3 seconds after it's ready, just for the sake of the example."""
+    await ctx.send("Bye")
+    await asyncio.sleep(3)
+    await bot.close()
+
+DISCORD_TOKEN = config('DISCORD_TOKEN')
+
+bot.run(DISCORD_TOKEN)
 
