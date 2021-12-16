59194fffcbee30c1dc5ec34340a6daa00e60c8a9
JasonSather <jassather@gmail.com>
2021-12-16 15:30:51 -0800

    Now inherits from face detector and camera

--- src/imageProcessing/smartCamera.py
+++ src/imageProcessing/smartCamera.py
@@ -1,18 +1,37 @@
+from io import BytesIO
 from camera import Camera
 from face_detector import FaceFinder
 
 
 
-class smartCamera():
+class smartCamera(Camera, FaceFinder):
     def __init__(self) -> None:
         '''Constructor for smart Camera
 
         
         '''
-        self.camera = Camera()
-        self.faceDetector = FaceFinder()
+        # self.camera = Camera()
+        # self.faceDetector = FaceFinder()
         self.bareFaces = 0          #number of non-masked faces the camera detects
         self.maskedFaces = 0        #number of masked faces that the camera detects
+        self.stream = self.getPictureStream()          #Cached image ig
+        
 
-    def analyzeForFaces(self):
-        stream = self.camera.getPictureStream()
+    def purgeStream(self):
+        self.stream.seek(0)
+        self.stream.truncate(0)
+
+    def getNewPicture(self):
+        self.purgeStream()
+        self.stream = self.getPictureStream()
+
+    def analyzeForBareFaces(self, newPicture = False): 
+        if newPicture:
+            self.getNewPicture()
+        #else use the old picture, whatever that maybe
+        self.bareFaces = self.getNumFacesFromImage(self.stream)
+        #make a way to extract the pic that open cv makes...
+
+
+
+        
