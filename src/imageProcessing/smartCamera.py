fba2de4285bddda125c66a1d499d642e48ca768e
JasonSather <jassather@gmail.com>
2021-12-16 16:09:05 -0800

    preparing to handle processed images

--- src/imageProcessing/smartCamera.py
+++ src/imageProcessing/smartCamera.py
@@ -14,23 +14,38 @@ class smartCamera(Camera, FaceFinder):
         # self.faceDetector = FaceFinder()
         self.bareFaces = 0          #number of non-masked faces the camera detects
         self.maskedFaces = 0        #number of masked faces that the camera detects
-        self.stream = self.getPictureStream()          #Cached image ig
+        self.basePicture = self.getPictureStream()          #Cached image ig
+        self.processedPhotoBare = None
+        self.processedPhotoMasked = None
         
 
     def purgeStream(self):
-        self.stream.seek(0)
-        self.stream.truncate(0)
+        self.basePicture.seek(0)
+        self.basePicture.truncate(0)
 
     def getNewPicture(self):
         self.purgeStream()
-        self.stream = self.getPictureStream()
+        self.basePicture = self.getPictureStream()
 
     def analyzeForBareFaces(self, newPicture = False): 
         if newPicture:
             self.getNewPicture()
         #else use the old picture, whatever that maybe
-        self.bareFaces = self.getNumFacesFromImage(self.stream)
+        self.bareFaces = self.getNumFacesFromImage(self.basePicture)
         #make a way to extract the pic that open cv makes...
+
+    def updateFaceCount(self, newPicture = False):
+        self.analyzeForBareFaces(newPicture)
+        #self.analyzeForMaskedFaces(newPicture)
+        
+
+    def getNumBareFaces(self):
+        return self.bareFaces
+    
+    def getNumMaskedFaces(self):
+        return self.maskedFaces
+
+    
 
 
 
