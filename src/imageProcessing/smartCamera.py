654e667847224900aecc4b070c54f16993064ded
JasonSather <jassather@gmail.com>
2021-12-16 18:36:11 -0800

    updated flow of how to use smartCam

--- src/imageProcessing/smartCamera.py
+++ src/imageProcessing/smartCamera.py
@@ -14,29 +14,28 @@ class smartCamera(Camera, FaceFinder):
         # self.faceDetector = FaceFinder()
         self.bareFaces = 0          #number of non-masked faces the camera detects
         self.maskedFaces = 0        #number of masked faces that the camera detects
-        self.basePicture = self.getPictureStream()          #Cached image ig
-        self.processedPhotoBare = None
-        self.processedPhotoMasked = None
+        self.basePicture = BytesIO()          #Cached image ig
+        self.processedPhotoBare = BytesIO()
+        self.processedPhotoMasked = BytesIO() 
         
 
     def purgeStream(self):
         self.basePicture.seek(0)
         self.basePicture.truncate(0)
 
-    def getNewPicture(self):
+    def getNewPicture(self):  #gets a new pic and then performs analysis on them...
         self.purgeStream()
         self.basePicture = self.getPictureStream()
+        self.updateFaceCount()
 
-    def analyzeForBareFaces(self, newPicture = False): 
-        if newPicture:
-            self.getNewPicture()
+    def analyzeForBareFaces(self): 
         #else use the old picture, whatever that maybe
         self.bareFaces = self.getNumFacesFromImage(self.basePicture)
         #make a way to extract the pic that open cv makes...
 
-    def updateFaceCount(self, newPicture = False):
-        self.analyzeForBareFaces(newPicture)
-        #self.analyzeForMaskedFaces(newPicture)
+    def updateFaceCount(self):
+        self.analyzeForBareFaces()
+        #self.analyzeForMaskedFaces()
         
 
     def getNumBareFaces(self):
