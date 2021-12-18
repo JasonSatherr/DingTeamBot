4da459cf8784a0650631a6b694933c86ff2a8f7b
JasonSather <jassather@gmail.com>
2021-12-18 14:21:19 -0800

    class to test the settings of the camera

---
+++ src/imageProcessing/cameraTester.py
@@ -0,0 +1,14 @@
+import time
+import picamera
+
+class cameraTester:
+    def __init__(self) -> None:
+        pass
+    def runAWBTests(self):
+        with picamera.PiCamera() as camera:
+            camera.resolution = (1024, 768)
+            for mode in camera.AWB_MODES:
+                camera.awb_mode = mode
+                time.sleep(2)
+                camera.capture('./photos/'+str(mode)+'foo.jpeg')
+                print("snap")
