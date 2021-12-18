1c3836986351cfecddd0c90e77603b0e0b48b84c
JasonSather <jassather@gmail.com>
2021-12-18 10:38:55 -0800

    trimmed excess comments

--- src/main.py
+++ src/main.py
@@ -4,15 +4,9 @@ from discord_bot import DiscordBot
 def main():
     """The main file will run everything
     """
-    # cam = Camera()
-    # stream = cam.getPictureStream()
-    # faceFinder = FaceFinder()
-    # numFaces = faceFinder.getNumFacesFromImage(stream)
-    # print('we found '+ str(numFaces)+ ' faces')
 
-    #Start the discord bot...
     discordBot = DiscordBot()
-    #discordBot.giveDripPic(stream)
+
     discordBot.start()
     
 
